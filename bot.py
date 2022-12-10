3cfcd0c01f948510768d9bb71567c076c45eadcd
Silver Phantom <117503544+SilverPhantomRi@users.noreply.github.com>
2022-12-10 16:26:55 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,43 @@
+#導入Discord.py
+import discord
+#client 是我們與 Discord 連結的橋樑，intents 是我們要求的權限
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+#調用event函式庫
+@client.event
+#當機器人完成啟動時
+async def on_ready():
+    print('目前登入身份：',client.user)
+    game = discord.Game('努力學習py中')
+    #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
+    await client.change_presence(status=discord.Status.idle, activity=game)
+    
+@client.event
+#當有訊息時
+async def on_message(message):
+    #排除自己的訊息，避免陷入無限循環
+    if message.author == client.user:
+        return
+    #如果以「說」開頭
+    if message.content.startswith('說'):
+      #分割訊息成兩份
+      tmp = message.content.split(" ",2)
+      #如果分割後串列長度只有1
+      if len(tmp) == 1:
+        await message.channel.send("你要我說什麼啦？")
+      else:
+        await message.channel.send(tmp[1])
+    if message.content.startswith('更改狀態'):
+        #切兩刀訊息
+        tmp = message.content.split(" ",2)
+        #如果分割後串列長度只有1
+        if len(tmp) == 1:
+            await message.channel.send("你要改成什麼啦？")
+        else:
+            game = discord.Game(tmp[1])
+            #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
+            await client.change_presence(status=discord.Status.idle, activity=game)
+
+client.run('MTA1MDY5MzE4NjYzMDIwNTUxMQ.G8xjo4.ZJO8JbltNR8VNoW2C3MrfdmvTKEyM32xfdUCaw') #TOKEN在剛剛Discord Developer那邊「BOT」頁面裡面
